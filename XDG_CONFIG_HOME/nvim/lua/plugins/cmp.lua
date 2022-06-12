210b0f82bb43a157c9d527873cd260d83f2029c4
Edward Guan <nooop3.lau@gmail.com>
2022-06-12 20:26:45 +0800

    nvim: add <C-e> to abort cmp

--- XDG_CONFIG_HOME/nvim/lua/plugins/cmp.lua
+++ XDG_CONFIG_HOME/nvim/lua/plugins/cmp.lua
@@ -21,6 +21,7 @@ cmp.setup {
     ["<C-u>"] = cmp.mapping.scroll_docs(-4),
     -- ["<C-Space>"] = cmp.mapping.complete(),
     ["<C-d>"] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.abort(),
     ["<CR>"] = cmp.mapping.confirm {
       behavior = cmp.ConfirmBehavior.Replace,
       -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
