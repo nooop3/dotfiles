c7a7546711425cb328c2017c153b735495506d50
Edward Guan <nooop3.lau@gmail.com>
2022-10-19 09:35:25 +0800

    nvim: Fix plugins warnings

--- XDG_CONFIG_HOME/nvim/lua/plugins/lspconfig.lua
+++ XDG_CONFIG_HOME/nvim/lua/plugins/lspconfig.lua
@@ -5,7 +5,7 @@ local util = require("lspconfig").util
 
 -- Add additional capabilities supported by nvim-cmp
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
+capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
 local opts = { noremap=true, silent=true }
 
