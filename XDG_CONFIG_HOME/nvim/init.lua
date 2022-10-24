c9d44c278c2d1310892d7d3535368effc6590ef1
Edward Guan <nooop3.lau@gmail.com>
2022-10-24 20:50:11 +0800

    nvim: update nvim-tree plugin settings

--- XDG_CONFIG_HOME/nvim/init.lua
+++ XDG_CONFIG_HOME/nvim/init.lua
@@ -6,6 +6,11 @@
 vim.g.mapleader = ";"
 vim.g.localleader = "\\"
 
+-- nvim-tree
+-- disable netrw at the very start of your init.lua (strongly advised)
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
 -- IMPORTS
 require("variables")
 require("options")
