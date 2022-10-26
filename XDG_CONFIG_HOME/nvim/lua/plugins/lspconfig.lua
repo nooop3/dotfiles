ca9e96b041a116f91975a6b6724504cbd2ceedb0
Edward Guan <nooop3.lau@gmail.com>
2022-10-26 22:59:54 +0800

    nvim: fix gotmpl filetype detect

--- XDG_CONFIG_HOME/nvim/lua/plugins/lspconfig.lua
+++ XDG_CONFIG_HOME/nvim/lua/plugins/lspconfig.lua
@@ -100,6 +100,7 @@ local servers = {
 	"bashls",
 	-- npm install -g typescript typescript-language-server
 	"tsserver",
+	-- sudo pacman -S gopls
 	"gopls",
 	-- sudo pacman -S lua-language-server
 	-- brew install lua-language-server
