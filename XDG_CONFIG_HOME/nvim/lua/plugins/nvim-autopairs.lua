0174881dd45bc417a57aabe83acd7e216a848eea
Edward Guan <nooop3.lau@gmail.com>
2022-10-26 22:02:33 +0800

    nivm: update nvim-autopairs plugin

--- XDG_CONFIG_HOME/nvim/lua/plugins/nvim-autopairs.lua
+++ XDG_CONFIG_HOME/nvim/lua/plugins/nvim-autopairs.lua
@@ -6,6 +6,12 @@ end
 
 autopairs.setup({
 	disable_filetype = { "TelescopePrompt" },
+	check_ts = true,
+	ts_config = {
+		lua = { "string" }, -- it will not add a pair on that treesitter node
+		javascript = { "template_string" },
+		java = false, -- don't check treesitter on java
+	},
 	map_cr = true,
 	fast_wrap = {
 		map = "<M-e>",
@@ -18,3 +24,18 @@ autopairs.setup({
 		highlight_grey = "Comment",
 	},
 })
+
+-- import nvim-autopairs completion functionality safely
+local cmp_autopairs_setup, cmp_autopairs = pcall(require, "nvim-autopairs.completion.cmp")
+if not cmp_autopairs_setup then
+	return
+end
+
+-- import nvim-cmp plugin safely (completions plugin)
+local cmp_setup, cmp = pcall(require, "cmp")
+if not cmp_setup then
+	return
+end
+
+-- make autopairs and completion work together
+cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
