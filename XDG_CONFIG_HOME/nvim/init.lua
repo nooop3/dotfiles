95b70e33fdf6cb00f7739e46221dc62a77aedbfe
Edward Guan <nooop3.lau@gmail.com>
2022-11-10 13:22:18 +0800

    nvim: add pg_format extra_args

--- XDG_CONFIG_HOME/nvim/init.lua
+++ XDG_CONFIG_HOME/nvim/init.lua
@@ -11,6 +11,13 @@ vim.g.localleader = "\\"
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
+-- netrw
+-- vim.g.netrw_banner = 0
+-- vim.g.netrw_liststyle = 3
+-- vim.g.netrw_browse_split = 4
+-- vim.g.netrw_altv = 1
+-- vim.g.netrw_winsize = 25
+
 -- IMPORTS
 require("variables")
 require("options")
