687c5601c2438f8a766b58bc5086bd2c66f193b6
Edward Guan <nooop3.lau@gmail.com>
2022-11-12 17:57:45 +0800

    nvim: fix neovide settings

--- XDG_CONFIG_HOME/nvim/lua/neovide.lua
+++ XDG_CONFIG_HOME/nvim/lua/neovide.lua
@@ -6,12 +6,12 @@ local has = vim.fn.has
 local exists = vim.fn.exists
 
 if exists("g:neovide") then
-	if has("mac") then
+	if has("mac") == 1 then
 		opt.guifont = "SauceCodePro Nerd Font"
-	elseif has("linux") then
-		opt.guifont = "Source Code Pro"
+	elseif has("linux") == 1 then
+		opt.guifont = "Source Code Pro:h12"
 	end
-	-- g.neovide_fullscreen = true
+	g.neovide_fullscreen = true
 	g.neovide_remember_window_size = true
 	-- g.neovide_cursor_vfx_mode = "railgun"
 end
