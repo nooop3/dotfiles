bc4c764d99365fca59188ef46b61e71c144077e7
Edward Guan <nooop3.lau@gmail.com>
2023-02-26 18:26:59 +0800

    kitty: disable startup session

--- XDG_CONFIG_HOME/kitty/dump-session.sh
+++ XDG_CONFIG_HOME/kitty/dump-session.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
+# vim:noswapfile:number
 
 set -eou pipefail
 
-#kitty @ ls | ./kitty-convert-dump.py > session.conf
+# kitty @ ls | ./kitty-convert-dump.py > session.conf
 
 kitty @ ls |  "$HOME/.config/kitty/kitty-convert-dump.py" > "$HOME/.config/kitty/session.conf"
 
