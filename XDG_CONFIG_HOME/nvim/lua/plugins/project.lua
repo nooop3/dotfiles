66abe10aa32aa4951cbbf99ab5285bdb14e8e3cf
Edward Guan <nooop3.lau@gmail.com>
2023-03-26 23:11:49 +0800

    nvim: fix project_nvim config

--- XDG_CONFIG_HOME/nvim/lua/plugins/project.lua
+++ XDG_CONFIG_HOME/nvim/lua/plugins/project.lua
@@ -1,13 +1,18 @@
-
 return {
-  "ahmedkhalf/project.nvim",
-  -- opts = {
-  --   detection_methods = { "pattern", "lsp" },
-  --   -- patterns = { ".git", "_darcs", ".hg", ".bzr", ".svn", "Makefile", "package.json" },
-  --   ignore_lsp = {},
-  --   exclude_dirs = { "~/.cargo/*" },
-  --   show_hidden = true,
-  --   silent_chdir = false,
-  --   scope_chdir = "tab",
-  -- }
+	{
+		"ahmedkhalf/project.nvim",
+		version = false,
+		config = function()
+			local project = require("project_nvim")
+			project.setup({
+				detection_methods = { "pattern", "lsp" },
+				-- patterns = { ".git", "_darcs", ".hg", ".bzr", ".svn", "Makefile", "package.json" },
+				ignore_lsp = {},
+				exclude_dirs = { "~/.cargo/*" },
+				show_hidden = true,
+				silent_chdir = false,
+				scope_chdir = "tab",
+			})
+		end,
+	},
 }
