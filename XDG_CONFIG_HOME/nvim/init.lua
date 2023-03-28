271dd38993585a444acf9180ee4a082ebf4fd4cf
Edward Guan <nooop3.lau@gmail.com>
2023-03-28 15:24:30 +0800

    nvim: update options

--- XDG_CONFIG_HOME/nvim/init.lua
+++ XDG_CONFIG_HOME/nvim/init.lua
@@ -1,29 +1,10 @@
 -- inspired by LazyVim https://www.lazyvim.org/
 -- nvim --headless "+Lazy! sync" +qa
 
--- LEADER
--- These keybindings need to be defined before the first /
--- is called; otherwise, it will default to "\"
-vim.g.mapleader = ";"
-vim.g.localleader = "\\"
-
+require("config.options")
 require("config.lazy")
 
--- nvim-tree
--- disable netrw at the very start of your init.lua (strongly advised)
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-
--- netrw
--- vim.g.netrw_banner = 0
--- vim.g.netrw_liststyle = 3
--- vim.g.netrw_browse_split = 4
--- vim.g.netrw_altv = 1
--- vim.g.netrw_winsize = 25
-
 -- IMPORTS
-require("variables")
-require("options")
 require("keymaps")
 require("autocmd")
 require("neovide")
