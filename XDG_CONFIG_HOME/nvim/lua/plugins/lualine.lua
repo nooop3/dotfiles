88445a909bd100e4541815a79572651aba87af20
Edward Guan <nooop3.lau@gmail.com>
2023-03-31 11:52:21 +0800

    nvim: update plugins options

--- XDG_CONFIG_HOME/nvim/lua/plugins/lualine.lua
+++ XDG_CONFIG_HOME/nvim/lua/plugins/lualine.lua
@@ -5,33 +5,21 @@ return {
   opts = {
     options = {
       icons_enabled = true,
-      -- theme = "auto",
-      theme = "gruvbox-material",
+      theme = "auto",
+      -- theme = "gruvbox-material",
       -- theme = "dracula-nvim",
-      component_separators = { left = "", right = "" },
-      section_separators = { left = "", right = "" },
       disabled_filetypes = {
-        statusline = { "packer", "NvimTree" },
+        statusline = { "packer", "NvimTree", "lazy" },
         winbar = {},
       },
-      ignore_focus = {},
-      always_divide_middle = true,
-      globalstatus = false,
-      refresh = {
-        statusline = 1000,
-        tabline = 1000,
-        winbar = 1000,
-      },
+      ignore_focus = { "packer", "NvimTree", "lazy" },
+      globalstatus = true,
     },
     sections = {
       lualine_a = { "mode" },
       lualine_b = { "branch", "diff", "diagnostics" },
       lualine_c = {
-        {
-          "filename",
-          file_status = true,
-          path = 1,
-        },
+        { "filename", file_status = true, path = 1 },
       },
       lualine_x = { "encoding", "fileformat", "filetype" },
       lualine_y = { "progress" },
@@ -54,6 +42,6 @@ return {
     tabline = {},
     winbar = {},
     inactive_winbar = {},
-    extensions = {},
+    extensions = { "nvim-tree", "quickfix" },
   },
 }
--- XDG_CONFIG_HOME/nvim/lua/plugins/nvim-autopairs.lua
+++ XDG_CONFIG_HOME/nvim/lua/plugins/nvim-autopairs.lua
@@ -3,21 +3,15 @@ return {
   event = "VeryLazy",
   opts = function()
     local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-
-    -- import nvim-cmp plugin safely (completions plugin)
     local cmp = require("cmp")
-
-    -- make autopairs and completion work together
     cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
     return {
-      disable_filetype = { "TelescopePrompt" },
       check_ts = true,
       ts_config = {
-        lua = { "string" }, -- it will not add a pair on that treesitter node
+        lua = { "string" },
         javascript = { "template_string" },
-        java = false, -- don't check treesitter on java
+        java = false,
       },
-      map_cr = true,
       fast_wrap = {
         map = "<M-e>",
         chars = { "{", "[", "(", '"', "'" },
