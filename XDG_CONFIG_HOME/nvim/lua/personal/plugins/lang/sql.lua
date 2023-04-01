5746bbc7800d9f8466fa9264b19fd9eb095096e4
Edward Guan <nooop3.lau@gmail.com>
2023-04-01 17:57:48 +0800

    nvim: add SQL language support

---
+++ XDG_CONFIG_HOME/nvim/lua/personal/plugins/lang/sql.lua
@@ -0,0 +1,40 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, { "sql" })
+      end
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, { "sqlfluff" })
+    end,
+  },
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    opts = function(_, opts)
+      local nls = require("null-ls")
+      vim.list_extend(opts.sources, {
+        nls.builtins.diagnostics.sqlfluff.with({
+          extra_args = { "--dialect", "postgres" }, -- change to your dialect
+        }),
+        nls.builtins.formatting.sqlfluff.with({
+          extra_args = { "--dialect", "postgres" }, -- change to your dialect
+        }),
+        -- nls.builtins.formatting.pg_format.with({
+        --   extra_args = {
+        --     "--keep-newline",
+        --     "--no-extra-line",
+        --     "--redshift",
+        --     "--keyword-case=0",
+        --     -- "--extra-function=" .. vim.env.HOME .. "/.config/pg_format/functions.lst",
+        --     "--extra-function=" .. vim.fn.expand("$HOME/.config/pg_format/functions.lst"),
+        --   },
+        -- }),
+      })
+    end,
+  },
+}
