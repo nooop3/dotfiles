ad3591558c92b6bb0edcb0c03cfcd5edf47c8983
Edward Guan <nooop3.lau@gmail.com>
2023-04-01 20:41:31 +0800

    nvim: add YAML support

---
+++ XDG_CONFIG_HOME/nvim/lua/personal/plugins/lang/yaml.lua
@@ -0,0 +1,25 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, { "yaml" })
+      end
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, { "yamllint" })
+    end,
+  },
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    opts = function(_, opts)
+      local nls = require("null-ls")
+      vim.list_extend(opts.sources, {
+        nls.builtins.diagnostics.yamllint,
+      })
+    end,
+  },
+}
