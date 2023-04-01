8270a55dc88eba71aa205c57cc47c4a4a4ed22fe
Edward Guan <nooop3.lau@gmail.com>
2023-04-01 16:20:17 +0800

    nvim: add eslint

--- XDG_CONFIG_HOME/nvim/lua/personal/lazy-config.lua
+++ XDG_CONFIG_HOME/nvim/lua/personal/lazy-config.lua
@@ -32,6 +32,7 @@ require("lazy").setup({
     { import = "personal.plugins.lang.json" },
     { import = "personal.plugins.lang.rust" },
     -- { import = "personal.plugins.formatting.prettier" },
+    { import = "personal.plugins.linting.eslint" },
   },
   defaults = {
     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
