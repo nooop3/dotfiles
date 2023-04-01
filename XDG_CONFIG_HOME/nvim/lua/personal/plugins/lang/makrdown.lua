b8282416b061d4aaf4983c0f551a4744f7fcc675
Edward Guan <nooop3.lau@gmail.com>
2023-04-01 20:52:15 +0800

    nvim: add Markdown support

---
+++ XDG_CONFIG_HOME/nvim/lua/personal/plugins/lang/makrdown.lua
@@ -0,0 +1,25 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, { "markdown", "markdown_inline" })
+      end
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, { "vale" })
+    end,
+  },
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    opts = function(_, opts)
+      local nls = require("null-ls")
+      vim.list_extend(opts.sources, {
+        nls.builtins.diagnostics.vale,
+      })
+    end,
+  },
+}
