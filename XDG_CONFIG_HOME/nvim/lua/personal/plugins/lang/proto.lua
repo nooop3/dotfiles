bd56f4f7b11c6cee1e6f82adf50aea38a189cfcb
Edward Guan <nooop3.lau@gmail.com>
2023-04-01 20:39:05 +0800

    nvim: add proto support

---
+++ XDG_CONFIG_HOME/nvim/lua/personal/plugins/lang/proto.lua
@@ -0,0 +1,26 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, { "proto" })
+      end
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, { "protolint" })
+    end,
+  },
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    opts = function(_, opts)
+      local nls = require("null-ls")
+      vim.list_extend(opts.sources, {
+        nls.builtins.diagnostics.protolint,
+        nls.builtins.formatting.protolint,
+      })
+    end,
+  },
+}
