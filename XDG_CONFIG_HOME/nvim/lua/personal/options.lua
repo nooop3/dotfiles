28ef3fe51b8a61d8898fe2cf3b46bbaf9889fd3b
Edward Guan <nooop3.lau@gmail.com>
2023-04-02 11:16:44 +0800

    nvim: schema enable italic

--- XDG_CONFIG_HOME/nvim/lua/personal/options.lua
+++ XDG_CONFIG_HOME/nvim/lua/personal/options.lua
@@ -22,6 +22,8 @@ vim.g.loaded_netrwPlugin = 1
 vim.g.gruvbox_material_background = "hard"
 vim.g.gruvbox_material_better_performance = 1
 vim.g.gruvbox_material_enable_bold = 1
+vim.g.gruvbox_material_enable_italic = 1
+-- vim.g.gruvbox_material_dim_inactive_windows = 1
 
 opt.autowrite = true -- Enable auto write
 -- opt.clipboard = "unnamedplus" -- Sync with system clipboard
