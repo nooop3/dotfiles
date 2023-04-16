1938b18aba1874d5248b4e2a3038b71463485cfc
Edward Guan <nooop3.lau@gmail.com>
2023-04-16 18:54:02 +0800

    neovim: add metals language server protocol

--- XDG_CONFIG_HOME/nvim/lua/personal/plugins/lang/java.lua
+++ XDG_CONFIG_HOME/nvim/lua/personal/plugins/lang/java.lua
@@ -7,4 +7,19 @@ return {
       end
     end,
   },
+  {
+    "scalameta/nvim-metals",
+    ft = { "scala", "sbt", "java" },
+    config = function()
+      local metals_config = require("metals").bare_config()
+      metals_config.settings = {
+        showImplicitArguments = true,
+        excludedPackages = { "akka.actor.typed.javadsl", "com.github.swagger.akka.javadsl" },
+      }
+
+      metals_config.capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+      require("metals").initialize_or_attach(metals_config)
+    end,
+  },
 }
