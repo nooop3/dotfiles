0b625bcfa6b51cf60084f923e47f2e85f19b5672
Edward Guan <nooop3.lau@gmail.com>
2023-05-28 10:27:28 +0800

    nvim: add ruby syntax highlight

--- XDG_CONFIG_HOME/nvim/lua/personal/plugins/lspsaga.lua
+++ XDG_CONFIG_HOME/nvim/lua/personal/plugins/lspsaga.lua
@@ -28,14 +28,14 @@ return {
       {
         "[E",
         function()
-          require("lspsaga.diagnostic").goto_prev({ severity = vim.diagnostic.severity.ERROR })
+          require("lspsaga.diagnostic"):goto_prev({ severity = vim.diagnostic.severity.ERROR })
         end,
         desc = "Prev error diagnostics",
       },
       {
         "]E",
         function()
-          require("lspsaga.diagnostic").goto_prev({ severity = vim.diagnostic.severity.ERROR })
+          require("lspsaga.diagnostic"):goto_next({ severity = vim.diagnostic.severity.ERROR })
         end,
         desc = "Next error diagnostics",
       },
