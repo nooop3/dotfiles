13cca2b768eb633ea7928f53c47158b40888e7a9
Edward Guan <nooop3.lau@gmail.com>
2023-08-11 12:03:26 +0800

    nvim: fix require cmp_nvim_lsp

--- XDG_CONFIG_HOME/nvim/lua/personal/plugins/lang/java.lua
+++ XDG_CONFIG_HOME/nvim/lua/personal/plugins/lang/java.lua
@@ -17,7 +17,14 @@ return {
         excludedPackages = { "akka.actor.typed.javadsl", "com.github.swagger.akka.javadsl" },
       }
 
-      metals_config.capabilities = require("cmp_nvim_lsp").default_capabilities()
+      local has_cmp, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
+      local capabilities = vim.tbl_deep_extend(
+        "force",
+        {},
+        vim.lsp.protocol.make_client_capabilities(),
+        has_cmp and cmp_nvim_lsp.default_capabilities() or {}
+      )
+      metals_config.capabilities = capabilities
 
       require("metals").initialize_or_attach(metals_config)
     end,
