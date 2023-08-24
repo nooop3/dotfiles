3545dc2306bd510e0c3c31310e6ef38005891591
Edward Guan <nooop3.lau@gmail.com>
2023-08-24 22:32:52 +0800

    nvim: use typescript-tools

--- XDG_CONFIG_HOME/nvim/lua/personal/plugins/lang/typescript.lua
+++ XDG_CONFIG_HOME/nvim/lua/personal/plugins/lang/typescript.lua
@@ -15,95 +15,188 @@ return {
   -- correctly setup lspconfig
   {
     "neovim/nvim-lspconfig",
-    dependencies = { "jose-elias-alvarez/typescript.nvim" },
+    dependencies = { "pmizio/typescript-tools.nvim" },
     opts = {
       -- make sure mason installs the server
       servers = {
-        ---@type lspconfig.options.tsserver
         tsserver = {
-          keys = {
-            { "<leader>co", "<cmd>TypescriptOrganizeImports<CR>", desc = "Organize Imports" },
-            { "<leader>cR", "<cmd>TypescriptRenameFile<CR>", desc = "Rename File" },
-          },
-          settings = {
-            typescript = {
-              format = {
-                indentSize = vim.o.shiftwidth,
-                convertTabsToSpaces = vim.o.expandtab,
-                tabSize = vim.o.tabstop,
-              },
-            },
-            javascript = {
-              format = {
-                indentSize = vim.o.shiftwidth,
-                convertTabsToSpaces = vim.o.expandtab,
-                tabSize = vim.o.tabstop,
-                semicolons = "remove",
-              },
-            },
-            diagnostics = {
-              ignoredCodes = {
-                -- See https://github.com/microsoft/TypeScript/blob/master/src/compiler/diagnosticMessages.json for a full list of valid codes.
-                -- Could not find a declaration file for module '{0}'. '{1}' implicitly has an 'any' type.
-                7016,
-                -- File is a CommonJS module; it may be converted to an ES module.
-                80001,
-              },
-            },
-            completions = {
-              completeFunctionCalls = true,
-            },
-          },
-          capabilities = {
-            documentFormattingProvider = false,
-          },
-          init_options = {
-            hostInfo = "neovim",
-            disableAutomaticTypingAcquisition = false,
-            preferences = {
-              quotePreference = "single",
-              -- importModuleSpecifierEnding = "js",
-              importModuleSpecifierEnding = "auto",
-            },
-            -- tsserver = {
-            --   logDirectory = vim.fn.expand("~/.cache/nvim/"),
-            --   logVerbosity = "verbose",
-            -- },
-          },
-        },
-        eslint = {
-          settings = {
-            -- helps eslint find the eslintrc when it's placed in a subfolder instead of the cwd root
-            workingDirectory = { mode = "auto" },
-          },
+          -- settings = {
+          --   typescript = {
+          --     format = {
+          --       indentSize = vim.o.shiftwidth,
+          --       convertTabsToSpaces = vim.o.expandtab,
+          --       tabSize = vim.o.tabstop,
+          --     },
+          --   },
+          --   javascript = {
+          --     format = {
+          --       indentSize = vim.o.shiftwidth,
+          --       convertTabsToSpaces = vim.o.expandtab,
+          --       tabSize = vim.o.tabstop,
+          --       semicolons = "remove",
+          --     },
+          --   },
+          --   diagnostics = {
+          --     ignoredCodes = {
+          --       -- See https://github.com/microsoft/TypeScript/blob/master/src/compiler/diagnosticMessages.json for a full list of valid codes.
+          --       -- Could not find a declaration file for module '{0}'. '{1}' implicitly has an 'any' type.
+          --       7016,
+          --       -- File is a CommonJS module; it may be converted to an ES module.
+          --       80001,
+          --     },
+          --   },
+          --   completions = {
+          --     completeFunctionCalls = true,
+          --   },
+          -- },
+          -- capabilities = {
+          --   documentFormattingProvider = false,
+          -- },
+          -- init_options = {
+          --   hostInfo = "neovim",
+          --   disableAutomaticTypingAcquisition = false,
+          --   preferences = {
+          --     quotePreference = "single",
+          --     -- importModuleSpecifierEnding = "js",
+          --     importModuleSpecifierEnding = "auto",
+          --   },
+          --   -- tsserver = {
+          --   --   logDirectory = vim.fn.expand("~/.cache/nvim/"),
+          --   --   logVerbosity = "verbose",
+          --   -- },
+          -- },
         },
       },
       setup = {
         tsserver = function(_, opts)
-          require("typescript").setup({ server = opts })
-          return true
-        end,
-        eslint = function()
-          vim.api.nvim_create_autocmd("BufWritePre", {
-            callback = function(event)
-              if not require("personal.plugins.lsp.format").enabled() then
-                -- exit early if autoformat is not enabled
-                return
-              end
-
-              local client = vim.lsp.get_active_clients({ bufnr = event.buf, name = "eslint" })[1]
-              if client then
-                local diag = vim.diagnostic.get(event.buf, { namespace = vim.lsp.diagnostic.get_namespace(client.id) })
-                if #diag > 0 then
-                  vim.cmd("EslintFixAll")
-                end
-              end
-            end,
+          local api = require("typescript-tools.api")
+          require("typescript-tools").setup({
+            handlers = {
+              ["textDocument/publishDiagnostics"] = api.filter_diagnostics({
+                -- File is a CommonJS module; it may be converted to an ES module.
+                80001,
+                -- Ignore 'This may be converted to an async function' diagnostics.
+                -- 80006
+              }),
+            },
+            settings = {
+              expose_as_code_action = {
+                "fix_all",
+                "remove_unused",
+                "add_missing_imports",
+              },
+            },
           })
+          return true
         end,
       },
     },
   },
+
+  -- correctly setup lspconfig
+  -- {
+  --   "neovim/nvim-lspconfig",
+  --   dependencies = { "jose-elias-alvarez/typescript.nvim" },
+  --   opts = {
+  --     -- make sure mason installs the server
+  --     servers = {
+  --       ---@type lspconfig.options.tsserver
+  --       tsserver = {
+  --         keys = {
+  --           { "<leader>co", "<cmd>TypescriptOrganizeImports<CR>", desc = "Organize Imports" },
+  --           { "<leader>cR", "<cmd>TypescriptRenameFile<CR>", desc = "Rename File" },
+  --         },
+  --         settings = {
+  --           typescript = {
+  --             format = {
+  --               indentSize = vim.o.shiftwidth,
+  --               convertTabsToSpaces = vim.o.expandtab,
+  --               tabSize = vim.o.tabstop,
+  --             },
+  --           },
+  --           javascript = {
+  --             format = {
+  --               indentSize = vim.o.shiftwidth,
+  --               convertTabsToSpaces = vim.o.expandtab,
+  --               tabSize = vim.o.tabstop,
+  --               semicolons = "remove",
+  --             },
+  --           },
+  --           diagnostics = {
+  --             ignoredCodes = {
+  --               -- See https://github.com/microsoft/TypeScript/blob/master/src/compiler/diagnosticMessages.json for a full list of valid codes.
+  --               -- Could not find a declaration file for module '{0}'. '{1}' implicitly has an 'any' type.
+  --               7016,
+  --               -- File is a CommonJS module; it may be converted to an ES module.
+  --               80001,
+  --             },
+  --           },
+  --           completions = {
+  --             completeFunctionCalls = true,
+  --           },
+  --         },
+  --         capabilities = {
+  --           documentFormattingProvider = false,
+  --         },
+  --         init_options = {
+  --           hostInfo = "neovim",
+  --           disableAutomaticTypingAcquisition = false,
+  --           preferences = {
+  --             quotePreference = "single",
+  --             -- importModuleSpecifierEnding = "js",
+  --             importModuleSpecifierEnding = "auto",
+  --           },
+  --           -- tsserver = {
+  --           --   logDirectory = vim.fn.expand("~/.cache/nvim/"),
+  --           --   logVerbosity = "verbose",
+  --           -- },
+  --         },
+  --       },
+  --     },
+  --     setup = {
+  --       tsserver = function(_, opts)
+  --         -- require("typescript").setup({ debug = true, server = opts })
+  --         return true
+  --       end,
+  --     },
+  --   },
+  -- },
+
+  -- eslint
+  -- {
+  --   "neovim/nvim-lspconfig",
+  --   opts = {
+  --     servers = {
+  --       eslint = {
+  --         settings = {
+  --           -- helps eslint find the eslintrc when it's placed in a subfolder instead of the cwd root
+  --           workingDirectory = { mode = "auto" },
+  --         },
+  --       },
+  --     },
+  --     setup = {
+  --       eslint = function()
+  --         vim.api.nvim_create_autocmd("BufWritePre", {
+  --           callback = function(event)
+  --             if not require("personal.plugins.lsp.format").enabled() then
+  --               -- exit early if autoformat is not enabled
+  --               return
+  --             end
+
+  --             local client = vim.lsp.get_active_clients({ bufnr = event.buf, name = "eslint" })[1]
+  --             if client then
+  --               local diag = vim.diagnostic.get(event.buf, { namespace = vim.lsp.diagnostic.get_namespace(client.id) })
+  --               if #diag > 0 then
+  --                 vim.cmd("EslintFixAll")
+  --               end
+  --             end
+  --           end,
+  --         })
+  --       end,
+  --     },
+  --   },
+  -- },
+
   -- {
   --   "williamboman/mason.nvim",
   --   opts = function(_, opts)
@@ -113,7 +206,7 @@ return {
   {
     "jose-elias-alvarez/null-ls.nvim",
     opts = function(_, opts)
-      table.insert(opts.sources, require("typescript.extensions.null-ls.code-actions"))
+      -- table.insert(opts.sources, require("typescript.extensions.null-ls.code-actions"))
     end,
     -- opts = function(_, opts)
     --   local nls = require("null-ls")
