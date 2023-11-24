817627ed14087ff9b3e6017b8a4663ec79d60ed4
Edward Guan <nooop3.lau@gmail.com>
2023-11-24 22:59:00 +0800

    LazyVim: use gruvbox-material theme

--- XDG_CONFIG_HOME/LazyVim/lua/config/options.lua
+++ XDG_CONFIG_HOME/LazyVim/lua/config/options.lua
@@ -1,3 +1,10 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Colortheme gruvbox material
+vim.g.gruvbox_material_background = "hard"
+vim.g.gruvbox_material_better_performance = 1
+vim.g.gruvbox_material_enable_bold = 1
+vim.g.gruvbox_material_enable_italic = 1
+-- vim.g.gruvbox_material_dim_inactive_windows = 1
---
+++ XDG_CONFIG_HOME/LazyVim/lua/plugins/colorsscheme.lua
@@ -0,0 +1,12 @@
+return {
+  -- add gruvbox
+  { "sainnhe/gruvbox-material" },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox-material",
+    },
+  },
+}
