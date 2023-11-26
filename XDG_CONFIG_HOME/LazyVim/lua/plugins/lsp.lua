8eff624bee699a2326fb7427a679f1fc42168d16
Edward Guan <nooop3.lau@gmail.com>
2023-11-26 15:32:49 +0800

    LazyVim: add protolint auto_disable fix option

--- XDG_CONFIG_HOME/LazyVim/lua/plugins/lsp.lua
+++ XDG_CONFIG_HOME/LazyVim/lua/plugins/lsp.lua
@@ -5,6 +5,7 @@ return {
       servers = {
         clangd = {
           -- enabled = false,
+          -- disabled_filetypes = { "proto" },
           filetypes = { "c", "cpp", "objc", "objcpp", "cuda" },
         },
       },
