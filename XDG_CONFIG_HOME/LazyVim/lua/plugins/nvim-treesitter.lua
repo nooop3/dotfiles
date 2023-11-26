eacd310326ab8a6f770ca6c39f74322f5956bdb1
Edward Guan <nooop3.lau@gmail.com>
2023-11-26 19:18:52 +0800

    LazyVim: fix gotmpl treesitter parse

--- XDG_CONFIG_HOME/LazyVim/lua/plugins/nvim-treesitter.lua
+++ XDG_CONFIG_HOME/LazyVim/lua/plugins/nvim-treesitter.lua
@@ -2,13 +2,42 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     opts = function(_, opts)
+      local parsers = require("nvim-treesitter.parsers")
+
       vim.list_extend(opts.ensure_installed, {
         "proto",
         "java",
         "kotlin",
         "scala",
         "hocon",
+        "php",
+        "ruby",
       })
+      vim.incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<CR>",
+          scope_incremental = "<CR>",
+          node_incremental = "<TAB>",
+          node_decremental = "<S-TAB>",
+        },
+      }
+
+      local parser_configs = parsers.get_parser_configs()
+
+      local github_mirror = "https://github.com/"
+      ---@diagnostic disable-next-line: inject-field
+      parser_configs.gotmpl = {
+        install_info = {
+          url = "https://github.com/ngalaiko/tree-sitter-go-template",
+          files = { "src/parser.c" },
+        },
+        filetype = "gotmpl",
+        used_by = { "gohtmltmpl", "gotexttmpl", "gotmpl", "yaml" },
+      }
+      for _, config in pairs(parser_configs) do
+        config.install_info.url = config.install_info.url:gsub("https://github.com/", github_mirror)
+      end
     end,
   },
 }
