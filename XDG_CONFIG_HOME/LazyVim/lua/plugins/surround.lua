4e26919f439cf48a550a27c023130e99d19990ba
Edward Guan <nooop3.lau@gmail.com>
2023-11-27 14:45:51 +0800

    LazyVim: update mini.surround keymappings

---
+++ XDG_CONFIG_HOME/LazyVim/lua/plugins/surround.lua
@@ -0,0 +1,14 @@
+return {
+  "echasnovski/mini.surround",
+  opts = {
+    mappings = {
+      add = "csa",
+      delete = "csd",
+      find = "csf",
+      find_left = "csF",
+      highlight = "csh",
+      replace = "csr",
+      update_n_lines = "csn",
+    },
+  },
+}
