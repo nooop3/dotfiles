928ca187f22f7631f47291902a17be31f9e98d9f
Edward Guan <nooop3.lau@gmail.com>
2023-12-03 21:14:42 +0800

    LazyVim: add prehook for comment

--- XDG_CONFIG_HOME/LazyVim/lua/plugins/comment.lua
+++ XDG_CONFIG_HOME/LazyVim/lua/plugins/comment.lua
@@ -8,6 +8,7 @@ return {
     ft.set("hocon", { "#%s" })
     return {
       ignore = "^$",
+      pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
     }
   end,
 }
