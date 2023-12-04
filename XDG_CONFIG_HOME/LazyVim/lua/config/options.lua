20be375584bb652b91389717e520f0c7cfa76378
Edward Guan <nooop3.lau@gmail.com>
2023-12-04 13:53:49 +0800

    LazyVim: add neovide config

--- XDG_CONFIG_HOME/LazyVim/lua/config/options.lua
+++ XDG_CONFIG_HOME/LazyVim/lua/config/options.lua
@@ -12,3 +12,16 @@ vim.g.gruvbox_material_enable_italic = 1
 vim.opt.swapfile = false
 vim.opt.wrap = true
 vim.opt.clipboard:remove("unnamedplus")
+vim.opt.cursorcolumn = true
+
+--[[ neovide ]]
+if vim.g.neovide then
+  if vim.fn.has("mac") == 1 then
+    vim.opt.guifont = "SauceCodePro NF"
+  elseif vim.fn.has("linux") == 1 then
+    vim.opt.guifont = "Source Code Pro:h12"
+  end
+  -- vim.g.neovide_fullscreen = true
+  vim.g.neovide_remember_window_size = true
+  -- g.neovide_cursor_vfx_mode = "railgun"
+end
