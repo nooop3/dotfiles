6a40fc6e530d60bd866342f7288a743c57217e9e
Edward Guan <nooop3.lau@gmail.com>
2023-12-27 17:36:02 +0800

    nvim: add neogen plugin

--- XDG_CONFIG_HOME/LazyVim/lua/plugins/comment.lua
+++ XDG_CONFIG_HOME/LazyVim/lua/plugins/comment.lua
@@ -1,14 +1,57 @@
 return {
-  "numToStr/Comment.nvim",
-  event = "VeryLazy",
-  opts = function()
-    local ft = require("Comment.ft")
+  {
+    "numToStr/Comment.nvim",
+    event = "VeryLazy",
+    opts = function()
+      local ft = require("Comment.ft")
 
-    ft.set("sbt", { "//%s", "/*%s*/" })
-    ft.set("hocon", { "#%s" })
-    return {
-      ignore = "^$",
-      pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
-    }
-  end,
+      ft.set("sbt", { "//%s", "/*%s*/" })
+      ft.set("hocon", { "#%s" })
+      return {
+        ignore = "^$",
+        pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+      }
+    end,
+  },
+  -- neogen
+  {
+    "danymat/neogen",
+    dependencies = "nvim-treesitter/nvim-treesitter",
+    config = true,
+    -- Uncomment next line if you want to follow only stable versions
+    -- version = "*"
+    keys = {
+      {
+        "<leader>nf",
+        function()
+          require("neogen").generate({ type = "func" })
+        end,
+        desc = "Generate function docstring",
+      },
+      {
+        "<leader>nc",
+        function()
+          require("neogen").generate({ type = "class" })
+        end,
+        desc = "Generate class docstring",
+      },
+      {
+        "<leader>nt",
+        function()
+          require("neogen").generate({ type = "type" })
+        end,
+        desc = "Generate type docstring",
+      },
+      {
+        "<leader>nF",
+        function()
+          require("neogen").generate({ type = "file" })
+        end,
+        desc = "Generate file docstring",
+      },
+    },
+    opts = {
+      snippet_engine = "luasnip",
+    },
+  },
 }
