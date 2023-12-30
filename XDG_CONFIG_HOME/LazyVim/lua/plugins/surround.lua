68e0027d216e83b1445f901c80dfa94f9a756354
Edward Guan <nooop3.lau@gmail.com>
2023-12-30 22:41:54 +0800

    LazyVim: use vim-surround plugin

--- XDG_CONFIG_HOME/LazyVim/lua/plugins/surround.lua
+++ XDG_CONFIG_HOME/LazyVim/lua/plugins/surround.lua
@@ -1,14 +1,18 @@
 return {
-  "echasnovski/mini.surround",
-  opts = {
-    mappings = {
-      add = "csa",
-      delete = "csd",
-      find = "csf",
-      find_left = "csF",
-      highlight = "csh",
-      replace = "csr",
-      update_n_lines = "csn",
+  {
+    "echasnovski/mini.surround",
+    enabled = false,
+    opts = {
+      mappings = {
+        add = "csa",
+        delete = "csd",
+        find = "csf",
+        find_left = "csF",
+        highlight = "csh",
+        replace = "csr",
+        update_n_lines = "csn",
+      },
     },
   },
+  { "tpope/vim-surround" },
 }
