a6829c58e4419b02b5f8e0bbbcc32aed07d9ad65
Edward Guan <nooop3.lau@gmail.com>
2024-03-16 23:28:17 +0800

    LazyVim: fix javascript debug config

---
+++ XDG_CONFIG_HOME/LazyVim/lua/plugins/dap/typescript.lua
@@ -0,0 +1,23 @@
+return {
+  {
+    "mfussenegger/nvim-dap",
+    opts = function()
+      local dap = require("dap")
+      dap.adapters["pwa-node"] = {
+        type = "server",
+        -- https://github.com/mxsdev/nvim-dap-vscode-js/issues/46#issuecomment-1573554662
+        host = "127.0.0.1",
+        port = "${port}",
+        executable = {
+          command = "node",
+          -- 💀 Make sure to update this path to point to your installation
+          args = {
+            require("mason-registry").get_package("js-debug-adapter"):get_install_path()
+              .. "/js-debug/src/dapDebugServer.js",
+            "${port}",
+          },
+        },
+      }
+    end,
+  },
+}
