0013babb2bd07a6e1342d8d687ee3253c8e33485
Edward Guan <nooop3.lau@gmail.com>
2024-06-07 14:21:13 +0800

    LazyVim: update plugins

--- XDG_CONFIG_HOME/LazyVim/lua/plugins/disabled.lua
+++ XDG_CONFIG_HOME/LazyVim/lua/plugins/disabled.lua
@@ -1,5 +1,6 @@
 return {
   -- disable trouble
-  { "folke/trouble.nvim", enabled = false },
+  -- { "folke/trouble.nvim", enabled = false },
   { "echasnovski/mini.comment", enabled = false },
+  { "folke/flash.nvim", enabled = false },
 }
