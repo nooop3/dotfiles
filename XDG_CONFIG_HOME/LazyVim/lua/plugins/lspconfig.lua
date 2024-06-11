f08a5dfd79ef0fa17f68057827f1fb89279bb612
Edward Guan <nooop3.lau@gmail.com>
2024-06-11 16:10:07 +0800

    LazyVim: use vtsls instead of tsserver

--- XDG_CONFIG_HOME/LazyVim/lua/plugins/lspconfig.lua
+++ XDG_CONFIG_HOME/LazyVim/lua/plugins/lspconfig.lua
@@ -48,7 +48,7 @@ return {
           filetypes = { "c", "cpp", "objc", "objcpp", "cuda" },
         },
         tsserver = {
-          -- enabled = false,
+          enabled = false,
           init_options = {
             hostInfo = "neovim",
             disableAutomaticTypingAcquisition = false,
@@ -105,19 +105,66 @@ return {
             },
           },
         },
+        vtsls = {
+          -- enabled = true,
+          handlers = {
+            ["textDocument/publishDiagnostics"] = function(_, result, ctx, config)
+              if result.diagnostics == nil then
+                return
+              end
+
+              -- ignore some tsserver diagnostics
+              local idx = 1
+              while idx <= #result.diagnostics do
+                local entry = result.diagnostics[idx]
+
+                -- local formatter = require("format-ts-errors")[entry.code]
+                -- entry.message = formatter and formatter(entry.message) or entry.message
+
+                local ignoredCodes = {
+                  -- See https://github.com/microsoft/TypeScript/blob/main/src/compiler/diagnosticMessages.json for a full list of valid codes.
+                  -- Initializer provides no value for this binding element and the binding element has no default value.
+                  2525,
+                  -- Could not find a declaration file for module '{0}'. '{1}' implicitly has an 'any' type.
+                  7016,
+                  -- Parameter '{0}' implicitly has an '{1}' type, but a better type may be inferred from usage.
+                  7044,
+                  -- codes: https://github.com/microsoft/TypeScript/blob/main/src/compiler/diagnosticMessages.json
+                  -- File is a CommonJS module; it may be converted to an ES module.
+                  -- { message = "File is a CommonJS module; it may be converted to an ES module.", }
+                  80001,
+                }
+                local shouldIgnored = false
+                for _, value in ipairs(ignoredCodes) do
+                  if value == entry.code then
+                    shouldIgnored = true
+                    break
+                  end
+                end
+                if shouldIgnored then
+                  table.remove(result.diagnostics, idx)
+                else
+                  idx = idx + 1
+                end
+              end
+
+              vim.lsp.diagnostic.on_publish_diagnostics(_, result, ctx, config)
+            end,
+          },
+        },
         terraformls = {},
       },
       setup = {
-        tsserver = function(_, opts)
-          require("typescript").setup({ server = opts })
-          return true
-        end,
+        -- tsserver = function(_, opts)
+        --   require("typescript").setup({ server = opts })
+        --   return true
+        -- end,
         eslint = function()
           require("lazyvim.util").lsp.on_attach(function(client)
             if client.name == "eslint" then
               client.server_capabilities.documentFormattingProvider = true
-            elseif client.name == "tsserver" then
-              client.server_capabilities.documentFormattingProvider = false
+              -- elseif client.name == "tsserver" then
+              --   client.server_capabilities.documentFormattingProvider = false
             end
           end)
         end,
