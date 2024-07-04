848d1d8cc30e3c88ed01bb6191a5f7451238efdf
Edward Guan <nooop3.lau@gmail.com>
2024-07-04 10:52:46 +0800

    LazyVim: upgrade plugins

--- XDG_CONFIG_HOME/LazyVim/lua/config/lazy.lua
+++ XDG_CONFIG_HOME/LazyVim/lua/config/lazy.lua
@@ -27,9 +27,10 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.yaml" },
     { import = "lazyvim.plugins.extras.linting.eslint" },
+    { import = "lazyvim.plugins.extras.lsp.none-ls" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     { import = "lazyvim.plugins.extras.util.project" },
-    { import = "lazyvim.plugins.extras.lsp.none-ls" },
+    { import = "lazyvim.plugins.extras.editor.telescope" },
 
     -- import/override with your plugins
     { import = "plugins" },
