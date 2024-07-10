1b60fc39d15fa8fa8b2de3fecb848fc93af661a2
Edward Guan <nooop3.lau@gmail.com>
2024-07-10 10:26:37 +0800

    LazyVim: update treesitter keymaps

--- XDG_CONFIG_HOME/LazyVim/lua/plugins/nvim-treesitter.lua
+++ XDG_CONFIG_HOME/LazyVim/lua/plugins/nvim-treesitter.lua
@@ -35,10 +35,10 @@ return {
         incremental_selection = {
           enable = true,
           keymaps = {
-            init_selection = "<CR>",
-            scope_incremental = "<CR>",
-            node_incremental = "<TAB>",
-            node_decremental = "<S-TAB>",
+            init_selection = "gnn", -- set to `false` to disable one of the mappings
+            node_incremental = "grn",
+            scope_incremental = "grc",
+            node_decremental = "<bs>",
           },
         },
         indent = {
