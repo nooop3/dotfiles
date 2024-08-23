45ba7c382220481a1dc9cd92454ac886f1069a72
Edward Guan <nooop3.lau@gmail.com>
2024-08-23 16:52:50 +0800

    wezterm: add nightly config

---
+++ XDG_CONFIG_HOME/wezterm/theme_switcher.lua
@@ -0,0 +1,48 @@
+local wezterm = require("wezterm")
+local action = wezterm.action
+
+local M = {}
+
+M.theme_switcher = function(window, pane)
+	-- get builting color schemes
+	local schemes = wezterm.get_builtin_color_schemes()
+	local choices = {}
+	local config_path = wezterm.config_dir .. "/wezterm.lua"
+
+	-- populate theme names in choices list
+	for key, _ in pairs(schemes) do
+		table.insert(choices, { label = tostring(key) })
+	end
+
+	-- sort choices list
+	table.sort(choices, function(c1, c2)
+		return c1.label < c2.label
+	end)
+
+	window:perform_action(
+		action.InputSelector({
+			title = "🎨 Pick a Theme!",
+			choices = choices,
+			fuzzy = true,
+
+			-- execute 'sed' shell command to replace the line
+			-- responsible of colorscheme in my config
+			action = wezterm.action_callback(function(inner_window, inner_pane, _, label)
+				inner_window:perform_action(
+					action.SpawnCommandInNewTab({
+						args = {
+							"gsed",
+							"-i",
+							'/^Colorscheme/c\\Colorscheme = "' .. label .. '"',
+							config_path,
+						},
+					}),
+					inner_pane
+				)
+			end),
+		}),
+		pane
+	)
+end
+
+return M
