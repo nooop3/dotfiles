25d339aae8c0901644dfcbb1efdff53c5d55a843
Edward Guan <nooop3.lau@gmail.com>
2024-08-23 21:40:35 +0800

    Wezterm: add more configs
    Reference: https://mwop.net/blog/2024-07-04-how-i-use-wezterm.html

---
+++ XDG_CONFIG_HOME/wezterm/nvim-pane-navigation.lua
@@ -0,0 +1,57 @@
+local os = require("os")
+local wezterm = require("wezterm")
+
+local act = wezterm.action
+
+-- Wezterm <-> nvim pane navigation
+-- You will need to install https://github.com/aca/wezterm.nvim
+-- and ensure you export NVIM_LISTEN_ADDRESS per the README in that repo
+local move_around = function(window, pane, direction_wez, direction_nvim)
+	local result = os.execute("env NVIM_LISTEN_ADDRESS=/tmp/nvim" .. pane:pane_id() .. " " .. wezterm.home_dir .. "/.local/bin/wezterm.nvim.navigator" .. " " .. direction_nvim)
+	if result then
+		window:perform_action(act({ SendString = "\x17" .. direction_nvim }), pane)
+	else
+		window:perform_action(act({ ActivatePaneDirection = direction_wez }), pane)
+	end
+end
+
+wezterm.on("move-left", function(window, pane)
+	move_around(window, pane, "Left", "h")
+end)
+
+wezterm.on("move-right", function(window, pane)
+	move_around(window, pane, "Right", "l")
+end)
+
+wezterm.on("move-up", function(window, pane)
+	move_around(window, pane, "Up", "k")
+end)
+
+wezterm.on("move-down", function(window, pane)
+	move_around(window, pane, "Down", "j")
+end)
+
+local vim_resize = function(window, pane, direction_wez, direction_nvim)
+	local result = os.execute("env NVIM_LISTEN_ADDRESS=/tmp/nvim" .. pane:pane_id() .. " " .. wezterm.home_dir .. "/.local/bin/wezterm.nvim.navigator" .. " " .. direction_nvim)
+	if result then
+		window:perform_action(act({ SendString = "\x1b" .. direction_nvim }), pane)
+	else
+		window:perform_action(act({ ActivatePaneDirection = direction_wez }), pane)
+	end
+end
+
+wezterm.on("resize-left", function(window, pane)
+	vim_resize(window, pane, "Left", "h")
+end)
+
+wezterm.on("resize-right", function(window, pane)
+	vim_resize(window, pane, "Right", "l")
+end)
+
+wezterm.on("resize-up", function(window, pane)
+	vim_resize(window, pane, "Up", "k")
+end)
+
+wezterm.on("resize-down", function(window, pane)
+	vim_resize(window, pane, "Down", "j")
+end)
