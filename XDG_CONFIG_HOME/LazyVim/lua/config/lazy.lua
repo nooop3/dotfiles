eba359f38dde55f91eb4359a13148a496d3ea917
Edward Guan <nooop3.lau@gmail.com>
2024-08-29 10:39:14 +0800

    LazyVim: enable lang for helm

--- XDG_CONFIG_HOME/LazyVim/lua/config/lazy.lua
+++ XDG_CONFIG_HOME/LazyVim/lua/config/lazy.lua
@@ -21,6 +21,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.coding.codeium" },
     { import = "lazyvim.plugins.extras.dap.core" },
     -- { import = "lazyvim.plugins.extras.formatting.prettier" },
+    { import = "lazyvim.plugins.extras.lang.helm" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.ruby" },
     -- { import = "lazyvim.plugins.extras.lang.scala" },
