cd59f5879e9dd0eed1bafbb59f66b915b7b674aa
Edward Guan <nooop3.lau@gmail.com>
2024-09-23 17:51:01 +0800

    LazyVim: update yamlls schemas

--- XDG_CONFIG_HOME/LazyVim/lua/plugins/lspconfig.lua
+++ XDG_CONFIG_HOME/LazyVim/lua/plugins/lspconfig.lua
@@ -5,6 +5,17 @@ local yaml_setting = {
   validate = true,
   schemas = {
     kubernetes = "templates/**",
+    ["http://json.schemastore.org/github-workflow"] = ".github/workflows/*",
+    ["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
+    ["http://json.schemastore.org/ansible-stable-2.9"] = "roles/tasks/*.{yml,yaml}",
+    ["http://json.schemastore.org/prettierrc"] = ".prettierrc.{yml,yaml}",
+    ["http://json.schemastore.org/kustomization"] = "kustomization.{yml,yaml}",
+    ["http://json.schemastore.org/ansible-playbook"] = "*play*.{yml,yaml}",
+    ["http://json.schemastore.org/chart"] = "Chart.{yml,yaml}",
+    ["https://json.schemastore.org/dependabot-v2"] = ".github/dependabot.{yml,yaml}",
+    ["https://json.schemastore.org/gitlab-ci"] = "*gitlab-ci*.{yml,yaml}",
+    ["https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.1/schema.json"] = "*api*.{yml,yaml}",
+    ["https://raw.githubusercontent.com/argoproj/argo-workflows/master/api/jsonschema/schema.json"] = "*flow*.{yml,yaml}",
     ["https://raw.githubusercontent.com/docker/compose/master/compose/config/compose_spec.json"] = "{docker-,}compose*.{yml,yaml}",
   },
   schemaStore = {
