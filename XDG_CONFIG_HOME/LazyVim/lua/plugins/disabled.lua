798e1656b49a04a660fbc5070a3166230d265147
Edward Guan <nooop3.lau@gmail.com>
2024-09-23 17:03:37 +0800

    LazyVim: update yamlls setting

--- XDG_CONFIG_HOME/LazyVim/lua/plugins/disabled.lua
+++ XDG_CONFIG_HOME/LazyVim/lua/plugins/disabled.lua
@@ -3,4 +3,5 @@ return {
   -- { "folke/trouble.nvim", enabled = false },
   { "echasnovski/mini.comment", enabled = false },
   { "folke/flash.nvim", enabled = false },
+  -- { "b0o/SchemaStore.nvim", enabled = false },
 }
