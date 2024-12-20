c8408efabbc4e1471563f1620b862f9813ff0faa
Edward Guan <nooop3.lau@gmail.com>
2024-12-20 18:03:42 +0800

    nvim: remove nvim-cmp

--- XDG_CONFIG_HOME/LazyVim/lua/config/keymaps.lua
+++ XDG_CONFIG_HOME/LazyVim/lua/config/keymaps.lua
@@ -4,16 +4,6 @@
 
 local map = vim.keymap.set
 
-map("n", "<leader>1", ":1tabnext<CR>", { silent = true })
-map("n", "<leader>2", ":2tabnext<CR>", { silent = true })
-map("n", "<leader>3", ":3tabnext<CR>", { silent = true })
-map("n", "<leader>4", ":4tabnext<CR>", { silent = true })
-map("n", "<leader>5", ":5tabnext<CR>", { silent = true })
-map("n", "<leader>6", ":6tabnext<CR>", { silent = true })
-map("n", "<leader>7", ":7tabnext<CR>", { silent = true })
-map("n", "<leader>8", ":8tabnext<CR>", { silent = true })
-map("n", "<leader>9", ":9tabnext<CR>", { silent = true })
-
 -- quick select last changed block
 -- nnoremap <expr> gV '`[' . getregtype()[0] . '`]'
 map("n", "gV", [["`[" .. getregtype()[0] .. "`]"]], { expr = true, desc = "Quick select last changed block" })
