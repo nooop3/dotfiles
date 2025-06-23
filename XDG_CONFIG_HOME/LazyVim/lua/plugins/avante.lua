8a2f779e5d4943257400409fe7882940ca1142fa
Edward Guan <nooop3.lau@gmail.com>
2025-06-23 10:07:07 +0800

    LazyVim: update plugins

--- XDG_CONFIG_HOME/LazyVim/lua/plugins/avante.lua
+++ XDG_CONFIG_HOME/LazyVim/lua/plugins/avante.lua
@@ -5,18 +5,20 @@ return {
   -- enabled = false,
   opts = {
     provider = "gemini",
-    gemini = {
-      -- api_key_name = { "bw", "get", "notes", "gemini-api-key" },
-      -- endpoint = "https://generativelanguage.googleapis.com/v1beta/models",
-      -- model = "gemini-1.5-flash-latest",
-      -- model = "gemini-2.0-flash",
-      -- timeout = 30000,
-      -- temperature = 0,
-      -- max_tokens = 8192,
-    },
-    claude = {
-      -- https://github.com/yetone/avante.nvim/issues/1775
-      api_key_name = { "echo", "nothing" },
+    providers = {
+      gemini = {
+        -- api_key_name = { "bw", "get", "notes", "gemini-api-key" },
+        -- endpoint = "https://generativelanguage.googleapis.com/v1beta/models",
+        -- model = "gemini-1.5-flash-latest",
+        -- model = "gemini-2.0-flash",
+        -- timeout = 30000,
+        -- temperature = 0,
+        -- max_tokens = 8192,
+      },
+      claude = {
+        -- https://github.com/yetone/avante.nvim/issues/1775
+        api_key_name = { "echo", "nothing" },
+      },
     },
   },
   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
