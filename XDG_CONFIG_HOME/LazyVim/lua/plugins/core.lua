8200071d91fae7e728864463a640d2786b89ee5a
Edward Guan <nooop3.lau@gmail.com>
2025-11-11 16:27:47 +0800

    LazyVim: update stacks.nvim remote patterns

--- XDG_CONFIG_HOME/LazyVim/lua/plugins/core.lua
+++ XDG_CONFIG_HOME/LazyVim/lua/plugins/core.lua
@@ -14,6 +14,24 @@ return {
     "folke/snacks.nvim",
     opts = {
       gitbrowse = {
+        remote_patterns = {
+          -- custom open url
+          { "^git@ssh%.(.+):(.+)%.git$", "https://git.%1/%2" },
+
+          { "^(https?://.*)%.git$", "%1" },
+          { "^git@(.+):(.+)%.git$", "https://%1/%2" },
+          { "^git@(.+):(.+)$", "https://%1/%2" },
+          { "^git@(.+)/(.+)$", "https://%1/%2" },
+          { "^org%-%d+@(.+):(.+)%.git$", "https://%1/%2" },
+          { "^ssh://git@(.*)$", "https://%1" },
+          { "^ssh://([^:/]+)(:%d+)/(.*)$", "https://%1/%3" },
+          { "^ssh://([^/]+)/(.*)$", "https://%1/%2" },
+          { "ssh%.dev%.azure%.com/v3/(.*)/(.*)$", "dev.azure.com/%1/_git/%2" },
+          { "^https://%w*@(.*)", "https://%1" },
+          { "^git@(.*)", "https://%1" },
+          { ":%d+", "" },
+          { "%.git$", "" },
+        },
         url_patterns = {
           -- default to gitlab url patterns
           ["git%..+"] = {
