fbf77ff687dfc9bf7d0ea152428786b506fcfc15
Edward Guan <nooop3.lau@gmail.com>
2025-11-11 15:44:23 +0800

    LazyVim: upgrade plugins

--- XDG_CONFIG_HOME/LazyVim/lua/plugins/disabled.lua
+++ XDG_CONFIG_HOME/LazyVim/lua/plugins/disabled.lua
@@ -1,7 +1,7 @@
 return {
   -- disable trouble
   -- { "folke/trouble.nvim", enabled = false },
-  { "echasnovski/mini.comment", enabled = false },
+  -- { "nvim-mini/mini.comment", enabled = false },
   { "folke/flash.nvim", enabled = false },
   -- { "b0o/SchemaStore.nvim", enabled = false },
 }
--- XDG_CONFIG_HOME/LazyVim/lua/plugins/nvim-treesitter.lua
+++ XDG_CONFIG_HOME/LazyVim/lua/plugins/nvim-treesitter.lua
@@ -15,14 +15,6 @@ return {
         "just",
       })
 
-      local parser_configs = parsers.get_parser_configs()
-
-      local github_mirror = "https://github.com/"
-      for _, config in pairs(parser_configs) do
-        ---@diagnostic disable-next-line: undefined-field
-        config.install_info.url = config.install_info.url:gsub("https://github.com/", github_mirror)
-      end
-
       -- jinjia
 
       ---@diagnostic disable-next-line: inject-field
