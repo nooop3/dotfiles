dff9400ca91e86a7c0016f8713be9645d6b5cd24
Edward Guan <nooop3.lau@gmail.com>
2025-11-17 14:26:18 +0800

    LazyVim: read markdlownlint-cli2 config file

--- XDG_CONFIG_HOME/LazyVim/lua/plugins/markdown.lua
+++ XDG_CONFIG_HOME/LazyVim/lua/plugins/markdown.lua
@@ -1,15 +1,44 @@
 local markdown_ft = { "markdown", "norg", "rmd", "org", "vimwiki", "Avante" }
 
+local global_cfg = vim.fn.expand("~/.config/markdownlint/.markdownlint-cli2.jsonc")
+local cfg_args = vim.fn.filereadable(global_cfg) == 1 and { "--config", global_cfg } or {}
+
 return {
-  "OXY2DEV/markview.nvim",
-  enabled = false,
-  lazy = false,
-  ft = markdown_ft,
-  dependencies = {
-    "nvim-treesitter/nvim-treesitter",
-    "nvim-tree/nvim-web-devicons",
+  {
+    "OXY2DEV/markview.nvim",
+    enabled = false,
+    lazy = false,
+    ft = markdown_ft,
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-tree/nvim-web-devicons",
+    },
+    opts = {
+      filetypes = markdown_ft,
+    },
   },
-  opts = {
-    filetypes = markdown_ft,
+
+  -- LINTING (nvim-lint) with markdownlint-cli2
+  {
+    "mfussenegger/nvim-lint",
+    opts = function(_, opts)
+      opts.linters = {
+        ["markdownlint-cli2"] = {
+          prepend_args = cfg_args,
+        },
+      }
+    end,
+  },
+
+  -- FORMATTING (conform.nvim) using markdownlint-cli2 --fix
+  {
+    "stevearc/conform.nvim",
+    opts = function(_, opts)
+      opts.formatters = {
+        ["markdownlint-cli2"] = {
+          prepend_args = cfg_args,
+        },
+      }
+    end,
   },
 }
